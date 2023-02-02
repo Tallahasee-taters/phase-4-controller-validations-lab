5798334adb54fab8de0a0ef40f4c973c87839c50
Tate <thandley1234@gmail.com>
2023-02-01 17:05:00 -0700

    commit tree 1522adfc4674a798bbdf34e364d33596f8deed78

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+    rescue_from ActiveRecord::RecordInvalid, with: :handle_invalid
+
+    private
+
+    def handle_invalid(invalid)
+        render json: {errors: invalid.record.errors}, status: :unprocessable_entity
+    end
 end
--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -8,8 +8,14 @@ class AuthorsController < ApplicationController
 
   def create
     author = Author.create(author_params)
-
-    render json: author, status: :created
+    render json: author, status: :ok
+    # if author.valid?
+    #   render json: author, status: :created
+    # else
+    #   render json: {errors: "Invalid data - could not create author"}, status: :unprocessable_entity
+    # end
+  # rescue ActiveRcord::RecordInvalid => Invalid
+  #   render json: {errors: invalid.record.errors}, status: :unprocessable_entity
   end
 
   private
